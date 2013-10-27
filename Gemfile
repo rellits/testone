9e8b9feebfdc3345db09ae74e83452f75cc8d1bf
Christian Stiller <dev@rellits.com>
2013-10-27 05:47:23 +0100

    initialized

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+#ruby-gemset=railstutorial
+
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testone::Application.config.secret_key_base = 'c20450adbd61337a120740091a1a6538c7c1cd9a385f79a5e5b875bd692864269352ac82cfa08ac958164b8c230365d7b5d2812011f168c272b0a1591a0f1a87'
